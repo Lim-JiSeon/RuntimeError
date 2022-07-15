6d8735a3fa107f01b57b1c10241a549b47916605
Lim-JiSeon <83554018+Lim-JiSeon@users.noreply.github.com>
2022-07-16 00:45:08 +0900

    Fix image path

--- HUFSummer Hackathon/MZ세대/main.js
+++ HUFSummer Hackathon/MZ세대/main.js
@@ -1,7 +1,7 @@
 /* 7 image random change - background */ 
 let randomNumber = Math.floor(Math.random() * 7) + 1;
-document.querySelector('.img-box1 img').setAttribute('src', '/images/image' + randomNumber + '.jpg');
-document.querySelector('.img-box2 img').setAttribute('src', '/images/image' + randomNumber + '.jpg');
+document.querySelector('.img-box1 img').setAttribute('src', 'http://127.0.0.1:5500/HUFSummer%20Hackathon/MZ%EC%84%B8%EB%8C%80/images/image' + randomNumber + '.jpg');
+document.querySelector('.img-box2 img').setAttribute('src', 'http://127.0.0.1:5500/HUFSummer%20Hackathon/MZ%EC%84%B8%EB%8C%80/images/image' + randomNumber + '.jpg');
 
     
 /* page change */
