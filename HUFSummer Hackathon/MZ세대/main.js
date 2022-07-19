f02b90dce2a0a6eb70c04d0cfc4c3edcad143f17
yseojin1@gmail.com <yseojin1@gmail.com>
2022-07-20 02:26:17 +0900

    Update sub_input.html

--- HUFSummer Hackathon/MZ세대/main.js
+++ HUFSummer Hackathon/MZ세대/main.js
@@ -23,4 +23,9 @@ function resultBtn() {
 function saveBtn() {
     /* save 버튼을 클릭하면 result화면의 departure, destination 글씨가 입력받은 값으로 바뀜 */
     window.location.href = 'sub_result.html';
+    const start=document.getElementById('start').value;
+    document.getElementById("result1").innerText=start;
+
+    const arrive=document.getElementById('arrive').value;
+    document.getElementById("result2").innerText=arrive;
 };
